9dce8a5e4d6c94d9076f4aefa7397bd1680cfa65
Cameron Lau <cameron1998@gmail.com>
2016-02-14 01:23:20 -0800

    change

--- Website/server.js
+++ Website/server.js
@@ -3,22 +3,13 @@
 var express = require('express');
 var app = express();
 var path = require('path');
-var port = 80;
-
-app.use(express.static(path.join(__dirname+'/static/')));
+var port = 3000;
 
 app.get('/', function (req, res) {
-  console.log("client requested /");
   res.sendFile(path.join(__dirname+'/index.html'));
 });
-app.get('/search/colorvalue=ffffff', function (req, res){
-  console.log("client requested /search/colorvalue=ffffff");
-  res.sendFile(path.join(__dirname+'/white.html'));
-});
-
-app.get('/search/colorvalue=000000', function (req, res){
-  console.log("client requested /search/colorvalue=000000");
-  res.sendFile(path.join(__dirname+'/black.html'));
+app.get('/search/colorvalue: #*', function (req, res){
+  
 });
 
 app.listen(port);
