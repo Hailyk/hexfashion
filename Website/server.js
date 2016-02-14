bd15d4f1de335a8194b92e5c67b0f5c6e0d9b492
xeonjake <jiahaokuang7066@gmail.com>
2016-02-13 20:33:01 -0800

    website copied

--- Website/server.js
+++ Website/server.js
@@ -3,13 +3,15 @@
 var express = require('express');
 var app = express();
 var path = require('path');
-var port = 3000;
+var port = 80;
 
 app.get('/', function (req, res) {
+  console.log("client requested /");
   res.sendFile(path.join(__dirname+'/index.html'));
 });
-app.get('/search/colorvalue: #*', function (req, res){
-
+app.get('/search/colorvalueffffff', function (req, res){
+  console.log("client requested /search/colorvalueffffff");
+  res.sendFile(path.join(__dirname+'/white.html'));
 });
 
 app.listen(port);
